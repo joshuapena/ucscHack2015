5c3a2fecac56c6360b9afdb3f1228453809efffc
Tristan <tilevi01@gmail.com>
2015-01-10 13:03:08 -0800

    time format
    added time format.. i hope this pushes

--- src/app.js
+++ src/app.js
@@ -57,6 +57,12 @@ var detectTimeLoop = function() {
 
 detectTimeLoop();
 
+var formatTime = function() {
+
+	var hour = ( ( date.getHours() % 12 != 0 ) ? date.getHours() % 12 : 12 )
+	return date.getHours() + ":" + date.getMinutes() .. " " + ( ( date.getHours() < 12 ) ? "AM" : "PM" )
+}
+
 // Sets up the alarms
 var alarmItems = createAlarmItems(alarms);
 
@@ -136,6 +142,7 @@ main.on('select', function(e) {
         card.on('click', 'select', function() {
             Vibe.vibrate('long');
             alarms.push({
+				time: formatTime(),
                 hour: date.getHours() + 1,
                 minute: date.getMinutes() + 2,
                 enabled: true
