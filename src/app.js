e045a0cf90d96a5902c5615e85ff103b364969b0
Joshua Pena <penacalata@gmail.com>
2015-01-09 21:40:34 -0800

    fixing bugs

--- src/app.js
+++ src/app.js
@@ -96,7 +96,6 @@ main.on('click', 'up', function(e) {
             }]
         }]
     });
-    */
     menu.on('select', function(e) {
         console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
         console.log('The item is titled "' + e.item.title + '"');
