df64f484993b949523e6e02a87a9872e4c954032
Joshua Pena <penacalata@gmail.com>
2015-01-10 22:26:05 -0800

    add one?

--- src/app.js
+++ src/app.js
@@ -290,7 +290,7 @@ main.on('select', function(e) {
                     // Disable or Enable the Alarm
                     for (var i = 0; i < alarms.length; i++) {
                         console.log(i + " : " + alarms[i].enabled);
-                        console.log(f.itemIndex + " : " + alarms[f.itemIndex - 0].enabled);
+                        console.log(f.itemIndex + 1 + " : " + alarms[f.itemIndex + 1].enabled);
                     }
                     alarms[f.itemIndex - 0].enabled = !alarms[f.itemIndex - 0].enabled;
                     break;
