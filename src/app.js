6f1e59b0e4cdd60760c93f625dca248b374f99bc
Joshua Pena <penacalata@gmail.com>
2015-01-10 22:40:08 -0800

    the magic 1

--- src/app.js
+++ src/app.js
@@ -292,9 +292,9 @@ main.on('select', function(e) {
                     // Disable or Enable the Alarm
                     for (var i = 0; i < alarms.length; i++) {
                         console.log(i + " : " + alarms[i].enabled);
-                        console.log(e.itemIndex - 2 + " : " + alarms[e.itemIndex - 2].enabled);
+                        console.log(e.itemIndex - 1 + " : " + alarms[e.itemIndex - 1].enabled);
                     }
-                    alarms[e.itemIndex - 0].enabled = !alarms[e.itemIndex - 0].enabled;
+                    alarms[e.itemIndex - 1].enabled = !alarms[e.itemIndex - 1].enabled;
                     break;
                 case 1:
                     // Edit the time of the alarm
