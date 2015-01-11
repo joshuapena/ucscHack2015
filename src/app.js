0ff86d601f878799d0a6a7e50a492ff838ad40e4
Joshua Pena <penacalata@gmail.com>
2015-01-10 22:18:51 -0800

    console.logs

--- src/app.js
+++ src/app.js
@@ -110,6 +110,9 @@ var detectTimeLoop = function() {
 						main.show();
 					}
                     console.log("count : " + count);
+                    if (count) {
+                        console.log("alarm has stopped");
+                    }
 				}
 				
 				
@@ -290,7 +293,7 @@ main.on('select', function(e) {
                     // Disable or Enable the Alarm
                     for (var i = 0; i < alarms.length; i++) {
                         console.log(i + " : " + alarms[i].enabled);
-                        console.log(f.indexItem + " : " + alarms.[f.itemIndex - 0].enabled);
+                        console.log(f.indexItem + " : " + alarms[f.itemIndex - 0].enabled);
                     }
                     alarms[f.itemIndex - 0].enabled = !alarms[f.itemIndex - 0].enabled;
                     break;
